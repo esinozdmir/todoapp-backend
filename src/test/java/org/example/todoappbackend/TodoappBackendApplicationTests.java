731f23d1ef2756c0058d8e808e34c8c06cbd7230
esinozdmir <esinozdemir2002@gmail.com>
2025-05-08 11:27:29 +0300

    todoapp created

---
+++ src/test/java/org/example/todoappbackend/TodoappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
